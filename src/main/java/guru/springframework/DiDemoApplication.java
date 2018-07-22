44b916cdcc89da45a3288e6d24b5e10d1fe8c36c
nmkip <juan.ignacio848@gmail.com>
2018-07-22 20:14:07 -0300

    Example

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,12 +1,17 @@
 package guru.springframework;
 
+import guru.springframework.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		// We asked the context for the bean. We didn't create an instance of MyController, it was all managed by the Spring Framework.
+		myController.hello();
 	}
 }
