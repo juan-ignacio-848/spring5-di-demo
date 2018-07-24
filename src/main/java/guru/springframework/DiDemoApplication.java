aadfe25899c0d1e127937a3718db7ca21772dec3
nmkip <juan.ignacio848@gmail.com>
2018-07-24 20:10:23 -0300

    Component scan example

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -7,8 +7,10 @@ import guru.springframework.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication //Component scan from THIS PACKAGE AND DOWN. => From guru.springframework and down
+@ComponentScan(basePackages = {"guru.services", "guru.springframework"}) // We refactored packages so that services is out of guru.springframework scope. So we have to change the ComponentScan. This is a base it will search under those packages.
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
