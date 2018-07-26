e135fd244f73115881f4d74a105e054a032013a1
nmkip <juan.ignacio848@gmail.com>
2018-07-25 21:43:26 -0300

    External properties example

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,38 @@
+package guru.springframework.config;
+
+import guru.springframework.services.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    private String username;
+
+    @Value("${guru.password}")
+    private String password;
+
+    @Value("${guru.dburl}")
+    private String dbUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setDbUrl(dbUrl);
+        return fakeDataSource;
+    }
+
+    // Isn't needed anymore. Spring is automatically doing this.
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer property() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+
+}
